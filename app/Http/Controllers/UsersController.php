6a9dd8b24107982a4555e7ac60a3c5944f9176f5
chaofu <chaofu123456@163.com>
2018-01-28 09:43:28 +0000

    add acount activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
@@ -12,7 +13,7 @@ class UsersController extends Controller
     {
         //除了这几个请求，其他都需要登录才能操作
         $this->middleware('auth',[
-            'except'=>['show','create','store','index']
+            'except'=>['show','create','store','index','confirmEmail']
         ]);
 
         $this->middleware('guest',[
@@ -62,9 +63,11 @@ class UsersController extends Controller
         ]);
         //session 保存session 因为是http协议是无状态的 所以 Laravel 提供了一种用于临时保存用户数据的方法 - 会话（Session），
         //并附带支持多种会话后端驱动，可通过统一的 API 进行使用。
-        Auth::login($user); //自动登录 注册成功
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+//        Auth::login($user); //自动登录 注册成功
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+//        return redirect()->route('users.show',[$user]);
+        return redirect('/');
     }
 
     /**
@@ -123,4 +126,30 @@ class UsersController extends Controller
         session()->flash('success','成功删除用户');
         return back();
     }
+
+    public function sendEmailConfirmationTo($user){
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'aufree@yousails.com';
+        $name = 'Aufree';
+        $to = $user->email;
+        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+    public function confirmEmail($token){
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+
+    }
+
 }
